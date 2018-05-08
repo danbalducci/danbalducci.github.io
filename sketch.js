8e2c158767282aa25649b4897f6aff264e2ea44a
danbalducci <32689920+danbalducci@users.noreply.github.com>
2018-05-08 12:24:06 -0700

    new

--- sketch.js
+++ sketch.js
@@ -1,31 +1,50 @@
-// var cap;
-// function setup() {
-//   createCanvas(400, 400);
-//   cap = createCapture(VIDEO);
-//   cap.hide();
-//   rectMode(CENTER);
-//   noStroke();
-// }
-// function draw() {
-//   background(50);
-//   fill(255);
-//   cap.loadPixels();
-//   for (var cy = 0; cy < cap.height; cy += 10) {
-//     for (var cx = 0; cx < cap.width; cx += 20) {
-//       var offset = ((cy*cap.width)+cx)*4;
-//       var xpos = (cx / cap.width) * width;
-//       var ypos = (cy / cap.height) * height;
-//       rect(xpos, ypos, 10,
-//         10 * (cap.pixels[offset+1]/255));
-//     }
-//   }
-// }
+
+var cap;
+var img;
+var offset = 0;
+var easing = 0.05;
+
 
 function setup() {
-  createCanvas(720, 400);
-  background(200);
-  fill(204, 101, 192, 127);
-  stroke(127, 63, 120);
-  rect(40, 120, 120, 40);
-  }
+ createCanvas(400, 400);
+ cap = createCapture(VIDEO);
+ cap.hide();
+ imageMode(CENTER);
+}
+function draw() {
+ background('#f7f5ef');
+ image(cap, mouseX, mouseY, 400, 400);
+ if (mouseX > 400) {
+ background('#5d7bb7');
+ image(cap, mouseX, mouseY, 400, 400);
+} else {
+
+}
+if (mouseX < 100) {
+background('#f7a313');
+image(cap, mouseX, mouseY, 400, 400);
+} else {
+
+}
+if (mouseIsPressed) {
+  image(cap, mouseX/2, mouseY, 1000, 1000);
+
+}
+
+ //if mouse is pressed
+
+
+
+ fill(0);
+cap.loadPixels();
+for (var cy = 0; cy < cap.height; cy += 2) {
+ for (var cx = 0; cx < cap.width; cx += 12) {
+   var offset = ((cy*cap.width)+cx)*4;
+   var xpos = (cx / cap.width) * width;
+   var ypos = (cy / cap.height) * height;
+   rect(xpos, ypos, 7,
+     1 * (cap.pixels[offset+2]/190))
+
+ }
+}
 }
